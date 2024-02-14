7eb082aaf5039d1d0e047d33fd6b97ec8e3f7751
謙蘭君 <hewun@lyceum.cc>
2024-02-14 19:48:53 +0800

    feat: add build script

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+rem 清空输出文件夹并创建
+rd /s /q ".\output" 2>nul
+mkdir ".\output"
+
+rem 遍历src文件夹下的.j文件
+for /r ".\src" %%i in (*.j) do (
+    rem 检查是否在lib文件夹内
+    echo %%i | findstr /I /C:"\\lib\\" >nul
+    if errorlevel 1 (
+        rem 在输出文件中添加文件名注释
+        echo///=========================================================================== >> ".\output\release.j"
+        echo/// filename is %%~nxi >> ".\output\release.j"
+        echo///=========================================================================== >> ".\output\release.j"
+        rem 将文件内容追加到输出文件中
+        type "%%i" >> ".\output\release.j"
+        rem 添加换行
+        echo. >> ".\output\release.j"
+    )
+)
